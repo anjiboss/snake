41cb508df5955f27d6af8f8db6256dc4dda20006
Anji <nhu.nhu.face@gmail.com>
2021-01-15 22:28:53 +0900

    done

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(function(){
     let x = 0;
     let y = 0;
+    let score = 0;
+    let hasFood = false;
+    food();
     $(document).keydown(function(e){
-        console.log(e.keyCode);
         if(e.keyCode == 38){
             if( y == 0){
                 y = 450;
@@ -12,7 +14,6 @@ $(function(){
             $("#a").css({
             "top": y  + "px"
             });
-            console.log(`x: ${x} y: ${y}`);
         }
         if(e.keyCode==37){
             if( x == 0){
@@ -23,7 +24,6 @@ $(function(){
             $("#a").css({
             "left": x + "px"
             });
-            console.log(`x: ${x} y: ${y}`);  
         }
         if(e.keyCode==39){
     
@@ -35,7 +35,6 @@ $(function(){
             $("#a").css({
             "left": x + "px"
             });
-            console.log(`x: ${x} y: ${y}`);
         }
         if(e.keyCode==40){
             if( y == 450){
@@ -46,12 +45,44 @@ $(function(){
             $("#a").css({
             "top": y  + "px"
             });
-            console.log(`x: ${x} y: ${y}`);
-
         }
-
+        if(isEat()){
+            console.log("****");
+            hasFood = false;
+            score++
+            $("#score").html(score);
+            food();
+        }
     });
+    function food(){
+        if (!hasFood){
+            ranFood();
+            hasFood = true;
+            setFood();
+        }
+    }
+    function ranFood(){
+        do{
+            f_x = Math.floor(Math.random() * 450);
+            f_y = Math.floor(Math.random() * 450);
+            if(f_x % 50 == 0 && f_y % 50 == 0){
+                break
+            }
+        }while(true);
+        console.log(`food X: ${f_x} food Y: ${f_y}`);
+    }
+    function setFood(){
+        $("#food").css({
+            "left": f_x + "px",
+            "top": f_y + "px"
+        });
+    }
+    function isEat(){
+        console.log(`x: ${x} y: ${y} f_x: ${f_x} f_y: ${f_y}`);
+        if (x == f_x && y == f_y){
+            return true;
+        }else {
+            return false;
+        }
+    }
 });
-// function ran_food(){
-//     f_x = 
-// }
